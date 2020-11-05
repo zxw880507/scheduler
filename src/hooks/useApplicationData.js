9f9bae6031d5a6d8a9ff100444fdd82eb7f6727a
Calvin Zheng <calvinzheng@Calvins-MBP.hitronhub.home>
2020-11-04 20:15:47 -0700

    Adding comments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -35,7 +35,7 @@ export default function useApplicationData() {
 
   useEffect(() => {
     const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
-    /* sync updates */
+    /* websocket sync updates */
     const eventHandler = (e) => {
       const data = JSON.parse(e.data);
       dispatch(data);
